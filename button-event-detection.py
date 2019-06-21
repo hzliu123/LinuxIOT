3a6186f008df65827ec0c19061696393e53522ad
Joseph Liu <hzliu123@gmail.com>
2019-06-21 15:48:51 +0800

    button-event-detection.py fix bugs

--- button-event-detection.py
+++ button-event-detection.py
@@ -6,14 +6,14 @@ GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(18, GPIO.OUT)
 
 def my_callback(channel):
-    print "falling edge detected on 4"
+    print("falling edge detected on 4")
 
 GPIO.add_event_detect(4, GPIO.FALLING, callback=my_callback, bouncetime=300)
 try:
     while(True):
-        GPIO.output(4, False)
+        GPIO.output(18, False)
         sleep(1)
-        GPIO.output(4, True)
+        GPIO.output(18, True)
         sleep(1)
 except KeyboardInterrupt:
     GPIO.cleanup() # clean up GPIO on CTRL+C exit
